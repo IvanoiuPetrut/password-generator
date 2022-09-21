67b3b0c651f453280239276fe4c83c9d906f0daa
Petrut <ivanoiu.petrut27@yahoo.com>
2022-09-21 18:04:48 +0300

    feat(style): add basic layout style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "accent-color": "#A4FFAF",
+        gray: {
+          500: "#929199",
+          700: "#23222A",
+          800: "#18171F",
+          900: "#0F0E14",
+        },
+      },
+    },
   },
   plugins: [],
 };
